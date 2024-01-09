5e5829ed7e057bfe92cdb17e65fe7440c8a26c8c
Ephyz8 <ziwoyaephraim@gmail.com>
2024-01-09 15:19:11 -0800

    reading from a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines function that reads a text file (UTF8) and prints it to stdout.
+"""
+def read_file(filename=""):
+    """Read the content of a text file and print it to stdout.
+
+    Args:
+        filename: str - The name of the file to read. If not provided, reads from stdin.
+
+    Returns:
+        None
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.rea())
