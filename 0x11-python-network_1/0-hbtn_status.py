10132c43fa0060d7bfd1072be87f501b3ec3a035
Ephraim <135040822+Ephyz8@users.noreply.github.com>
2024-03-31 03:47:28 +0200

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
       req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
       with urllib.request.urlopen(req) as response:
         body = response.read().decode('utf-8')
-        print("\t- Body response:")
-        print("\t\t- type:", type(body))
-        print("\t\t- content:", body)
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
    except urllib.error.URLError as e:
        print("Error fetching URL:", e)
