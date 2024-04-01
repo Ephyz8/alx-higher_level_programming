fded1b8a49921b470403637825b6ddf999167ea2
Ephraim <135040822+Ephyz8@users.noreply.github.com>
2024-04-02 01:55:04 +0200

    network 1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,6 +8,5 @@ from sys import argv
 
 if __name__ == '__main__':
     url = 'https://api.github.com/users/{}'.format(argv[1])
-    resp = requests.get(url,
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
+    resp = requests.get(url, auth=HTTPBasicAuth(argv[1], argv[2]))
     print(resp.json().get('id'))
